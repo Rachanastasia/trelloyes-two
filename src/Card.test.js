2ba617e15ae694987076478e63d06881f272e2cd
Rachel Reilly <r.a.reill18@gmail.com>
2020-08-25 14:56:20 -0500

    all tests passing

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './Card';
+import renderer from 'react-test-renderer';
 
+describe ('Card component', () =>{ 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Card />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
-  
+
+it('renders the UI as expected', () =>{
+  const tree = renderer
+      .create(<Card title="titletitle" content="contentcontent" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+
+})
+
+
+
+
+});
--- src/List.test.js
+++ src/List.test.js
@@ -3,28 +3,30 @@ import ReactDOM from 'react-dom';
 import List from './List';
 import renderer from 'react-test-renderer';
 
+describe('List component', () => {
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<List header="Testing testing test" cards={[{id: 2, name: 'hi'}, {id: 3, name: 'hi'}, {id: 4, name: 'hi'} ]} />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<List header="Testing testing test" cards={[{ id: 2, name: 'hi' }, { id: 3, name: 'hi' }, { id: 4, name: 'hi' }]} />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders the UI as expected', () => {
+        //jest.mock('TextInput', () => 'TextInput');
+        const tree = renderer
+            .create(<List header="Test" cards={[]} />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('renders the UI as expected', () => {
+        const tree = renderer
+            .create(<List header="Hi" cards={[{ id: 2, title: 'hi', content: 'hhh' }, { id: 3, title: 'hi', content: 'hhh' }, { id: 4, title: 'hi', content: 'hhh'}]} />)
+            .toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+});
 
 
-it('renders the UI as expected', () => {
-    const tree = renderer   
-        .create(<List header="Test" cards={[{id: 2, name: 'hi'}, {id: 3, name: 'hi'}, {id: 4, name: 'hi'} ]}/>)
-        .toJSON();
-    expect(tree).toMatchSnapshot();
-})
 
-it('renders the UI as expected', () => {
-    const tree = renderer   
-        .create(<List header="Test" cards={[]}/>)
-        .toJSON();
-    expect(tree).toMatchSnapshot();
-})
-
-
-
-  
