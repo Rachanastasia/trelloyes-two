958190f7954fd3a81754445db9f6d9b727d79ed3
Rachel Reilly <r.a.reill18@gmail.com>
2020-08-25 13:06:40 -0500

    changing

--- src/List.test.js
+++ src/List.test.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './List';
+import renderer from 'react-test-renderer';
 
-describe ('<List />', () =>
+
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<List />, div);
+    ReactDOM.render(<List header="Testing testing test" cards={[{id: 2, name: 'hi'}, {id: 3, name: 'hi'}, {id: 4, name: 'hi'} ]} />, div);
     ReactDOM.unmountComponentAtNode(div);
-  }));
+  });
+
+
+it('renders the UI as expected', () => {
+    const tree = renderer   
+        .create(<List header="Test" cards={[{id: 2, name: 'hi'}, {id: 3, name: 'hi'}, {id: 4, name: 'hi'} ]}/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+})
+
+it('renders the UI as expected', () => {
+    const tree = renderer   
+        .create(<List header="Test" cards={[]}/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+})
+
+
+
   
